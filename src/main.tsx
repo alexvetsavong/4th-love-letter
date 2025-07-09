56c4ea96ea934060fff19681fc9b3da35b44b86c
Vetsavong, Alex N <alex.n.vetsavong@lmco.com>
2025-07-09 16:27:55 -0500

    integrate react-router in data mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.tsx";
 import Navbar from "./Navbar.tsx";
 
+function Root() {
+  return (
+    <StrictMode>
+      <Navbar />
+      <div className="spacer"></div>
+      <App />
+    </StrictMode>
+  );
+}
+
+function Letter() {
+  return (
+    <StrictMode>
+      <Navbar />
+      <div className="spacer"></div>
+      <h1>Nothing to see here yet!</h1>
+    </StrictMode>
+  );
+}
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+  },
+  { path: "/letter", Component: Letter },
+]);
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Navbar />
-    <div className="spacer"></div>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
