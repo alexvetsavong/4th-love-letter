7d12203ef6f1b1d993c5fecbf336c6b090847f6b
Alex Vetsavong <alexvetsavong@gmail.com>
2025-07-10 17:48:59 -0500

    update for netlify deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./pages/pages.tsx";
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", Component: App },
   { path: "/letter", Component: App },
   { path: "/joke", Component: App },
