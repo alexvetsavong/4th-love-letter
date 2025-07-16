ddb0c77012941d188066768970b303ff9cd8081f
Vetsavong, Alex N <alex.n.vetsavong@lmco.com>
2025-07-16 16:42:49 -0500

    Revert "add vite-pwa plugin and build minimum manifest through config" because of breaking package manager lock, which prevents build.
    This reverts commit e1235990480809161ca46b404190355897cff58c.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
-  ],
+  plugins: [react()],
   base: "/",
   server: {
     watch: {
-      usePolling: true,
-    },
-  },
+      usePolling: true
+    }
+  }
 });
