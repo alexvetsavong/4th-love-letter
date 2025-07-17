7891e8a16e83ab030d33885af179d2e4c4323f6b
Alex Vetsavong <alexvetsavong@gmail.com>
2025-07-16 19:03:40 -0500

    add vite-plugin-pwa and set as dependency and config vite.config.ts for pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   base: "/",
   server: {
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
 });
